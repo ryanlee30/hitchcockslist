1fddb6cc011291a829c84b16aab8384ddd7b2c77
Ryan Lee <ryanptss@gmail.com>
2020-09-21 21:44:06 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+      this.callAPI();
+  }
+
+  render() {
+    return (    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </header>
+    </div>
+    );
+  }
+}
+
+export default App;
