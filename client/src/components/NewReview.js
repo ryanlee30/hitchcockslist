679dcd0c1b0705a47f41f0e766a2a763332606b6
Ryan Lee <ryanptss@gmail.com>
2021-02-15 19:03:32 -0800

    Added FilePond image uploader to review page

--- client/src/components/NewReview.js
+++ client/src/components/NewReview.js
@@ -1,10 +1,16 @@
 import '../App.css';
+import 'filepond/dist/filepond.min.css'
+
 import { React, useLayoutEffect, useState } from 'react';
 import SignOutFirstModal from './SignOutFirstModal';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory, useLocation, Link } from 'react-router-dom';
 import { Form, Row, Col } from 'react-bootstrap';
-import new_review from '../imgs/new_review.png';
 import Quill from 'quill';
+import { FilePond, registerPlugin } from 'react-filepond';
+
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 
 export default function NewReview() {
   const history = useHistory();
@@ -13,6 +19,8 @@ export default function NewReview() {
   const [filmTitle, setFilmTitle] = useState("");
   const [filmDirector, setFilmDirector] = useState("");
   const [filmArtwork, setFilmArtwork] = useState("");
+  const [reviewContent, setReviewContent] = useState({});
+  const [reviewEditor, setReviewEditor] = useState({});
 
   useLayoutEffect(() => {
     if (location.state !== undefined) {
@@ -23,13 +31,9 @@ export default function NewReview() {
 
     var toolbarOptions = [
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      [{ 'font': [] }],
       ['bold', 'italic', 'underline'],
       [{ 'align': [] }],
-      [{ 'indent': '+1' }, { 'indent': '-1'}],
-      ['blockquote'],
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      ['clean']
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }]
     ];
 
     var options = {
@@ -38,21 +42,32 @@ export default function NewReview() {
         toolbar: toolbarOptions
       }
     };
-  
+    
     var editor = new Quill("#text-editor", options);
+    setReviewEditor(editor);
   }, [location])
+
+  function logContent() {
+    var editor = reviewEditor;
+    var contentString = JSON.stringify(editor.getContents());
+    console.log(contentString);
+  }
+
+  registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
     return (
     <div>
-      <div className="banner">
+      <div className="review-banner">
         <div className="artwork-label">
           Poster | Artwork
         </div>
-        <input type="file" id="uploadFilmArtwork" onChange={e => setFilmArtwork(e.target.value)} hidden/>
-        <label for="uploadFilmArtwork" className="uploadFilmArtworkLabel">
-          <img src={new_review} className="now-showing"></img>
-          <div style={{width: "185px", height: "232px", backgroundColor: 'blue', position: "absolute", zIndex: 10, marginLeft: "11px", marginTop: "10px"}} hidden></div>
-        </label>
+        <FilePond
+          className="artwork-upload"
+          files={filmArtwork}
+          onupdatefiles={setFilmArtwork}
+          allowMultiple={false}
+          labelIdle='Drag & drop or <span class="filepond--label-action">Browse</span>'
+        />
         <div className="account-menu">
           <div className="account-name">Place Holder</div>
         </div>
@@ -60,7 +75,10 @@ export default function NewReview() {
       <div className="review-body">
         <div className="review-console">
           <div className="review-console-header-container">
-            <h5>About your review</h5>
+            <div className="review-console-header-head">
+              <h5>About your review</h5>
+              <Link to="#" className="redirect-btn-submit" style={{textDecoration: 'none', float: 'right'}} onClick={logContent}>Submit</Link>
+            </div>
             <div className="review-console-film-info">
               <Form.Group controlId="formFilmInfo">
                 <Row>
